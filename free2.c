78d590fef2becd294e1743ae352a095dc23ff087
Taylor Yang <rainbowww5@gmail.com>
2018-03-23 18:55:09 -0700

    test

--- free2.c
+++ free2.c
@@ -6,7 +6,7 @@
 /*   By: tyang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 11:36:01 by tyang             #+#    #+#             */
-/*   Updated: 2018/03/23 18:53:00 by tyang            ###   ########.fr       */
+/*   Updated: 2018/03/23 18:55:01 by tyang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,5 @@
 
 void	free_path(t_path *paths)
 {
-	t_path	*temp;
-
-	temp = NULL;
-	while (paths != NULL)
-	{
-		temp = paths;
-		free(paths);
-		paths = paths->next;
-	}
+	free(paths);
 }
